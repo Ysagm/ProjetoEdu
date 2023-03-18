ab22213ad5e05f4b0e885fb239a340bb68740dd1
SarahCapBarros <capellini.barros@gmail.com>
2023-03-17 22:22:27 -0300

    Isso vai quebrar, mas nao ligo

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,43 +7,9 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string nameOfLevel;
-    [SerializeField] private GameObject Menu;
-    [SerializeField] private GameObject Options;
-    //[SerializeField] private GameObject Extra;
-    [SerializeField] private GameObject Credits;
 
     public void Play(){
         SceneManager.LoadScene(nameOfLevel);
-    }
-    public void OpenOptions(){
-        Options.SetActive(true);
-        Debug.Log("Abriu opção");
-    }
-    public void CloseOptions(){
-        Menu.SetActive(true);       
-    }
-   /* public void OpenExtra(){
-        Menu.SetActive(false);
-        Extra.SetActive(true);
-        Debug.Log("Abriu extra");
-    }
-    public void CloseExtra(){
-        Extra.SetActive(false);
-        Menu.SetActive(true);
-    }*/
-
-    public void OpenCredits(){
-        Credits.SetActive(true);
-        Debug.Log("Abriu credits");
-    }
-    public void CloseCredits(){
-        Menu.SetActive(true);        
-    }
-
-    public void BackToMenu()
-    {
-        SceneManager.LoadScene("Menu");
-    }
-
+    }  
 }
 
