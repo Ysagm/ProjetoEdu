cda7890b6dac2fea44b60517771b271270a8964b
Ysadora G. M <ysagm@icloud.com>
2023-04-05 20:59:30 -0300

    wip

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,45 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    public GameObject tutorial;
-    static bool showTutorial = true;
-    //static Tutorial instance = null;
+    public Image imgOriginal;
+    public Sprite imgNext;
 
-    // Start is called before the first frame update
-    void Start()
+    public void NewImage()
     {
-        //PlayerPrefs.GetInt("showTutorial");
-
-        if (showTutorial)
-        {
-            StartCoroutine("StartDelay");
-            showTutorial = false;
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    IEnumerator StartDelay()
-    {
-        //Pause scene
-        Time.timeScale = 0;
-        //Star countdown after start
-        float pauseTime = Time.realtimeSinceStartup + 5f;
-        //Finish coutdown and Close tutorial
-        while (Time.realtimeSinceStartup < pauseTime)
-            yield return 0;
-        tutorial.gameObject.SetActive(false);
-        //Continue game
-        Time.timeScale = 1;
-    }
-
-    public static void Reset()
-    {
-        showTutorial = true;
+        imgOriginal.sprite = imgNext;
     }
 }
