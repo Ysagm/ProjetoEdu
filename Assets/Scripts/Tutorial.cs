698615f0ec9f7083ffe423adf1f99a2abd5f7eb8
Ysadora G. M <ysagm@icloud.com>
2023-04-26 14:18:17 -0300

    Tutorial p2

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -5,50 +5,35 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    //Img
     public Image imgOriginal;
-    //Tutorial Panel
     public GameObject tutorial;
-    //List of Sprites
     public Sprite[] imgNext;
-    //index
     private int currentIndex = 0;
-    
-    //Set PLayerPrefs
-    //Note: if you want to reset the tutorial and show it again, you can call the PlayerPrefs.DeleteKey(TUTORIAL_COMPLETED_KEY) 
-    //function to remove the tutorial_completed variable from storage. 
-    //This will cause the tutorial to be shown again the next time the scene is loaded.
-    private const string TUTORIAL_COMPLETED_KEY = "tutorial_completed";
+
+    private static bool tutorialCompleted = false;
 
     private void Start()
     {
-        // Check if tutorial has been completed before
-        if (PlayerPrefs.GetInt(TUTORIAL_COMPLETED_KEY, 0) == 1)
+        if (tutorialCompleted)
         {
-            // Tutorial has been completed, close the tutorial
             CloseTutorial();
         }
         else
         {
-            // Tutorial has not been completed, show the first image
             NewImage();
         }
     }
 
     public void NewImage()
     {
-        //check if current image index is less than the max of list
         if (currentIndex < imgNext.Length)
         {
             imgOriginal.sprite = imgNext[currentIndex];
-            //next img
             currentIndex++;
         }
         else
         {
-            // Tutorial has been completed, save the state and close the tutorial
-            // This will ensure that the tutorial is not shown again when the scene is reset.
-            PlayerPrefs.SetInt(TUTORIAL_COMPLETED_KEY, 1);
+            tutorialCompleted = true;
             CloseTutorial();
         }
     }
@@ -57,5 +42,10 @@ public class Tutorial : MonoBehaviour
     {
         // Implement the code to close the tutorial
         tutorial.SetActive(false);
+    }
+
+    public static void ResetTutorial()
+    {
+        tutorialCompleted = false;
     }
 }
